42ebd28f68465f188df66ce97137d2f725984251
npcoderes <nikulpanchal2505@gmail.com>
2025-08-01 14:20:06 +0530

    SERVER URL CHANGE

--- server/index.js
+++ server/index.js
@@ -1,28 +1,30 @@
 const express = require("express");
 require("dotenv").config();
-const {initializeTables} = require('./initTables');
-const authRoutes = require('./routes/auth.routes');
-const userRoutes = require('./routes/user.routes');
-const postRoutes = require('./routes/post.routes');
-const cors = require('cors');
+const { initializeTables } = require("./initTables");
+const authRoutes = require("./routes/auth.routes");
+const userRoutes = require("./routes/user.routes");
+const postRoutes = require("./routes/post.routes");
+const cors = require("cors");
 
 // Import models to trigger table creation
-require('./models/post.model');
-require('./models/comment.model');
-require('./models/like.model');
+require("./models/post.model");
+require("./models/comment.model");
+require("./models/like.model");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(cors({
-    origin: ['https://blog-app-front-one.vercel.app/'], 
+app.use(
+  cors({
+    origin: ["https://blog-app-front-one.vercel.app/", "http://localhost:5173"],
     credentials: true,
-}))
+  })
+);
 
 app.use(express.json());
-app.use('/api/auth', authRoutes);
-app.use('/api/user', userRoutes);
-app.use('/api/posts', postRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/user", userRoutes);
+app.use("/api/posts", postRoutes);
 
 app.get("/", (req, res) => {
   res.send("Hello, World!");
@@ -30,7 +32,7 @@ app.get("/", (req, res) => {
 
 async function startServer() {
   try {
-    initializeTables()
+    initializeTables();
     app.listen(PORT, () => {
       console.log(`Server is running on port ${PORT}`);
     });
