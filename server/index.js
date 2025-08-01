0a81845ab9302a5dc6678c6b241d2aa206ec7afc
npcoderes <nikulpanchal2505@gmail.com>
2025-08-01 14:14:57 +0530

    commit

--- server/index.js
+++ server/index.js
@@ -15,7 +15,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(cors({
-    origin: ['http://localhost:5173'], 
+    origin: ['https://blog-app-front-one.vercel.app/'], 
     credentials: true,
 }))
 
